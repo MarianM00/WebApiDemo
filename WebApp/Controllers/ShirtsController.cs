732381416041fe2627432fd92edd162c15e7e8b7
MarianM00 <marin_puiu99@yahoo.com>
2025-03-05 21:59:18 +0200

    Add mvc project

---
+++ WebApp/Controllers/ShirtsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+using WebApp.Models.Repositories;
+
+namespace WebApp.Controllers
+{
+    public class ShirtsController : Controller
+    {
+        private readonly IWebApiExecuter webApiExecuter;
+
+        public ShirtsController(IWebApiExecuter webApiExecuter)
+        {
+            this.webApiExecuter = webApiExecuter;
+        }
+        public  async Task <IActionResult> Index()
+        {
+           return View(await webApiExecuter.InvokeGet<List<Shirt>>("Shirts"));
+        }
+
+        public IActionResult CreateShirt()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateShirt(Shirt shirt)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await webApiExecuter.InvokePost<Shirt>("Shirts", shirt);
+                if (response != null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(shirt);
+        }
+
+        
+
+    }
+}
---
+++ WebApp/Data/IWebApiExecuter.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WebApp.Data
+{
+    public interface IWebApiExecuter
+    {
+        Task<T?> InvokeGet<T>(string relativeUrl);
+        Task<T?> InvokePost<T>(string relativeUrl, T obj);
+    }
+}
